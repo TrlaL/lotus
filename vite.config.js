01f1e5ec4542e062520075a018225ab8315ba813
Simkin Mark <marks.simkin@gmail.com>
2025-09-09 18:49:11 +0300

    vite-plugin-compression2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { compression } from 'vite-plugin-compression2'
 
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    compression()
   ],
   resolve: {
     alias: [
